49b4a59db974e4894e08bb307bdb01c747c1d460
Florent Girard <mrgirardflorent@gmail.com>
2022-10-06 10:21:35 +0200

    feat: add new folder for starting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <NuxtPage />
+    <slot />
   </div>
 </template>
+
 <script lang="ts" setup></script>
-<style lang="scss"></style>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ home.title }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const home = reactive({
+  title: 'hello',
+})
+</script>
+
+<style scoped></style>
